2cc4a44754b9a48d438fa3ae440db0394b40a172
collapsoid <collapsly@protonmail.com>
2020-07-09 18:50:40 +0300

    fix some ugly formatting

--- src/store/actions/addPlace.js
+++ src/store/actions/addPlace.js
@@ -13,20 +13,20 @@ const geocoder = (name, isUserDefined) => {
   return (dispatch, getState) => {
     const {YMapsAPI, mapInstance} = getState().YMaps;
 
-		const place = {
-			name,
-			isUserDefined
-		};
+    const place = {
+      name,
+      isUserDefined
+    };
 
     if (isUserDefined) {
-			const coordinates = mapInstance.getCenter();
+      const coordinates = mapInstance.getCenter();
 
       return dispatch(addPlace({...place, coordinates}));
     } else {
       return YMapsAPI.geocode(name).then(response => {
-        const coordinates = response.geoObjects.get(0).geometry._coordinates;
+	const coordinates = response.geoObjects.get(0).geometry._coordinates;
 
-        return dispatch(addPlace({...place, coordinates}));
+	return dispatch(addPlace({...place, coordinates}));
       });
     }
   };
--- src/store/actions/changeCoordinates.js
+++ src/store/actions/changeCoordinates.js
@@ -1,9 +1,9 @@
 const changeCoordinates = (name, coordinates) => ({
-	type: 'CHANGE_COORDINATES',
-	payload: {
-		name,
-		coordinates
-	}
+  type: 'CHANGE_COORDINATES',
+  payload: {
+    name,
+    coordinates
+  }
 });
 
 export default changeCoordinates;
